e3233d0ea76389c0649a4830603178d1a5b57ae8
Admin <pbelyi357@gmail.com>
2022-08-29 22:31:31 +0300

    commitPavel

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -37,8 +37,8 @@ module.exports = {
     // Generates an HTML file from a template
     // Generates deprecation warning: https://github.com/jantimon/html-webpack-plugin/issues/1501
     new HtmlWebpackPlugin({
-      title: 'webpack Boilerplate',
-      favicon: paths.src + '/images/favicon.png',
+      title: 'PizzaCV',
+      favicon: paths.src + '/images/favpizza.png',
       template: paths.src + '/template.html', // template file
       filename: 'index.html', // output file
     }),
