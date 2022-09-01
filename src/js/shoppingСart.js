8ea1ba3c1f986387328a10067c71c894bff8dec5
Admin <pbelyi357@gmail.com>
2022-09-01 18:54:33 +0300

    commitPavel

---
+++ src/js/shoppingСart.js
@@ -0,0 +1,7 @@
+let menu = document.querySelector(".pizzaNameGrid")
+let priceArr = menu.querySelectorAll(".valuePrice")
+let buttonsOrder = menu.querySelectorAll("button")
+// let elem = priceArr[0].querySelectorAll("p")
+console.log(Number(priceArr[0].textContent));
+console.log(buttonsOrder);
+// console.log(elem);
