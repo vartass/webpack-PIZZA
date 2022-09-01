28927835a6a17dd31cb20bd68b7c2ab267e75b2c
AmgBlack <lirik2104@msn.com>
2022-09-01 19:25:31 +0300

    kopija gotovogo proekta

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 // Test import of a JavaScript module
-import { example } from '@/js/example'
+import { example } from '@/../public/icon/js/example'
 
 // Test import of an asset
 import webpackLogo from '@/images/webpack-logo.svg'
 
 // Test import of styles
 import '@/styles/index.scss'
-import '@/js/shoppingСart'
+import '@/../public/icon/js/shoppingСart'
